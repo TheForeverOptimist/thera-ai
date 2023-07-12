e3b804a3e67517ccc5d5b5c6a1fdae6d927c830f
TheForeverOptimist <sean@elevate.app>
2023-07-12 12:54:30 -0400

    initial setup of thera-ai

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import AddBoxIcon from "@mui/icons-material/AddBox";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button>
+          <AddBoxIcon />
+        </button>
+        <ul className="history">
+          <li>Previous Entry</li>
+        </ul>
+        <nav>
+          <p>Made by the Team-1</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>Thera-AI</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit"></div>
+          </div>
+          <p className="info">
+            Thera-AI is not a substitute for a licensed professional
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
