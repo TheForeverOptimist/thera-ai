c426017509487ca1d3000749b171ca60dc2c4a4d
Edwin Rodriguez <edwinrodriguez@Edwins-MBP.attlocal.net>
2023-07-13 13:33:02 -0500

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 
-
-import { Sidebar } from "./components";
 import { Cover } from "./components";
 import { Chat } from "./components";
 import { Login } from "./components";
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-      <Login />
-      <Sidebar />
-      <Cover />
-      <Chat />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}></Route>
+      <Route path='/Home' element={<Cover/>}></Route>
+      <Route path='/Chat' element={<Chat/>}></Route>
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
